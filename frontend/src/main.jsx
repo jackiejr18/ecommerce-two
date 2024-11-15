949c4882731c372d72601d20800e371221ab243c
Jackie Jr <jwysinger@gmail.com>
2024-11-15 17:57:51 -0600

    addition of navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ReactDom.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -9,4 +9,3 @@ const Collection = () => {
 }
 
 export default Collection
-s

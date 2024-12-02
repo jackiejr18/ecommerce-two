c76d0fe8c5b36e28832cf2b0945204b27ae14885
Jackie Jr <jwysinger@gmail.com>
2024-12-02 10:17:50 -0600

    added more functionality to small side bar, completion of nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -52,10 +52,14 @@ const Navbar = () => {
         {/* Sidebar menu for small screens */}
         <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0' }`}>
           <div className='flex flex-col text-gray-600'>
-            <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+            <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
               <img className='h-4 rotate-180' src={assets.dropdown_icon} />
               <p>Back</p>
             </div>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/'>HOME</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/collection'>COLLECTION</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/about'>ABOUT</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 p1-6 border' to='/contact'>CONTACT</NavLink>
           </div>
         </div>
 

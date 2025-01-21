290780b5f7bbde0f0fe4fd0334a4040e2706e4ae
Jackie Jr <jwysinger@gmail.com>
2025-01-21 12:34:51 -0600

    fixed typo insort option

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -59,7 +59,7 @@ const Collection = () => {
       {/* Right Side  */}
       <div className='flex-1'>
 
-        <div className='flex justify-betweeen text-base sm:text-2xl mb-4'>
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
           {/* Product Sort */}
           <select className='border-2 border-gray-300 text-sm px-2'>

05a092f06363a9f4b64bd0f6bd5efd6a416418cf
Jackie Jr <jwysinger@gmail.com>
2025-01-21 11:14:05 -0600

    added products, need to get sort to right side of page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,12 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import Title from '../components/Title'
+import ProductItem from '../components/ProductItem';
+
 
 const Collection = () => {
 
 
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffect(()=>{
+    setFilterProducts(products);
+  },[])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -46,6 +54,30 @@ const Collection = () => {
             </p>
           </div>
         </div>
+      </div>
+
+      {/* Right Side  */}
+      <div className='flex-1'>
+
+        <div className='flex justify-betweeen text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'}/>
+          {/* Product Sort */}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value='relavent'>Sort by: Relavent</option>
+            <option value='low-high'>Sort by: Low to High</option>
+            <option value='high-low'>Sort by: High to Low</option>
+          </select>
+        </div>
+
+        {/* Map Products */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterProducts.map((item,index)=>(
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image}/>
+            ))
+          }
+        </div>
+
       </div>
       
     </div>

a3fb623f91eab1f2dd507de909632f27a6df4bd6
Jackie Jr <jwysinger@gmail.com>
2025-01-28 14:06:04 -0600

    addition of search bar to website

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
+
 
 export const ShopContext = createContext();
 
@@ -7,9 +8,12 @@ const ShopContextProvider = (props) => {
 
   const currency = '$';
   const delivery_fee = 10;
+  const [search, setSearch] = useState(''); 
+  const [showSearch, setShowSearch] = useState(true)
 
   const value = {
-    products, currency, delivery_fee
+    products, currency, delivery_fee,
+    search, setSearch, showSearch, setShowSearch
   }
 
   return (

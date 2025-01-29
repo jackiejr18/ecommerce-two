988d71c4a5c5c4c0a12321f63d5b61337323c552
Jackie Jr <jwysinger@gmail.com>
2025-01-29 08:50:21 -0600

    search bar is completely functional

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -8,7 +8,7 @@ import ProductItem from '../components/ProductItem';
 const Collection = () => {
 
 
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -40,6 +40,10 @@ const Collection = () => {
   const applyFilter = () => {
 
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -78,7 +82,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category, subCategory])
+  },[category, subCategory, search, showSearch])
 
   useEffect(()=>{
     sortProduct();

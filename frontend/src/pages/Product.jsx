f235957f3f531eaabfaae69b07c1f4834be7f07b
Jackie Jr <jwysinger@gmail.com>
2025-01-30 10:24:00 -0600

    logic for smaller image to enlarge on click

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -36,12 +36,12 @@ const Product = () => {
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map((item,index)=>(
-                <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'/>
+                <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'/>
               ))
             }
           </div>
           <div className='w-full sm:w-[80%]'>
-            <img src={image}/>
+            <img className='w-full h-auto' src={image}/>
           </div>
         </div>
       </div>

6a497eaf01f07876e2191f1bf94b62a5f9beb6be
Jackie Jr <jwysinger@gmail.com>
2025-02-05 09:03:00 -0600

    addition of payment method images

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
+import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
 
 const PlaceOrder = () => {
   return (
@@ -24,6 +26,32 @@ const PlaceOrder = () => {
           <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Country'/>
         </div>
         <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='number' placeholder='Phone'/>
+      </div>
+
+      {/* ------------------------- Right Side ----------------------- */}
+      <div className='mt-8'>
+        <div className='mt-8 min-w-80'>
+          <CartTotal/>
+        </div>
+
+        <div className='mt-12'>
+          <Title text1={'PAYMENT'} text2={'METHOD'}/>
+          {/* ----------------- Payment Method Selection -----------  */}
+          <div className='flex gap-3 flex-col lg:flex-row'>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <img className='h-5 mx-4' src={assets.stripe_logo}/>
+            </div>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <img className='h-5 mx-4' src={assets.razorpay_logo}/>
+            </div>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
